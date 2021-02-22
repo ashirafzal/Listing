bc32d72bdc5b377b679bef42ff5bfadd24f97b65
ashirafzal <ashirafzal96@gmail.com>
2021-02-22 00:18:14 -0800

    login and register page design changed.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,17 +8,17 @@
             <div class="row">
                 <div class="offset-xl-1 col-xl-10 offset-lg-1 col-lg-10 col-md-12 col-sm-12 col-12">
                     <!-- search-block -->
-                    <div class="">
+                    <div class="border p-4">
                         <div class="text-center search-head">
-                            <h1 class="search-head-title">Find Local Wedding Vendors</h1>
-                            <p class="d-none d-xl-block d-lg-block d-sm-block text-white">Browse the best wedding vendors in your area — from venues and photographers, to wedding planners, caterers, florists and more by registering yourself.</p>
+                            <h2 class="blue-text">Find Local Wedding Vendors</h2>
+                            <p class="d-none d-xl-block d-lg-block d-sm-block">Browse the best wedding vendors in your area — from venues and photographers, to wedding planners, caterers, florists and more by registering yourself.</p>
                         </div>
                         <!-- /.search-block -->
                         <!-- search-form -->
                         <form method="POST" action="{{ route('login') }}">
                             @csrf
                             <div class="form-group">
-                                <label for="email" class="font-weight-bold text-white">{{ __('E-Mail Address') }}</label>
+                                <label for="email" class="">{{ __('E-Mail Address') }}</label>
                             </div>
                             <div class="form-group">                              
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" autocomplete="email" autofocus>
@@ -29,7 +29,7 @@
                                 @enderror
                             </div>
                             <div class="form-group">
-                                <label for="password" class="font-weight-bold text-white">{{ __('Password') }}</label>
+                                <label for="password" class="">{{ __('Password') }}</label>
                             </div>                            
                             <div class="form-group">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
@@ -43,13 +43,13 @@
                                 <div class="form-check">
                                     <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
 
-                                    <label class="form-check-label text-white" for="remember">
+                                    <label class="form-check-label" for="remember">
                                         {{ __('Remember Me') }}
                                     </label>
                                 </div>
                             </div>
                             <div class="form-group">
-                                    <button type="submit" class="btn btn-danger btn-block">
+                                    <button type="submit" class="btn btn-primary btn-block">
                                         {{ __('Login') }}
                                     </button>
                             </div>
@@ -58,10 +58,10 @@
                         <br/>
                         <!-- Sign in text -->
                         <div class="text-center search-head">
-                            <p class="d-none d-xl-block d-lg-block d-sm-block text-white">
-                                <a class="text-white" href="/register">Don't have an account ? Register Yourelf</a>
+                            <p class="d-none d-xl-block d-lg-block d-sm-block">
+                                <a class="link" href="/register">Don't have an account ? Register Yourelf</a>
                                 <br/>
-                                <a class="text-white" href="{{ route('password.request') }}">{{ __('Forgot Your Password?') }}</a>
+                                <a class="link" href="{{ route('password.request') }}">{{ __('Forgot Your Password?') }}</a>
                             </p>
                         </div>
                         <!-- /. Sign in text -->
