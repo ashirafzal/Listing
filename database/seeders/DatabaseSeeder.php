bb7d227185196795ab29f5568143eae1115c4f1c
ashirafzal <ashirafzal96@gmail.com>
2021-02-25 01:06:38 -0800

    more fixes.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -10,7 +10,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             UserSeeder::class,
-            ListingSeeder::class,
+            // ListingSeeder::class,
         ]);
     }
 }
